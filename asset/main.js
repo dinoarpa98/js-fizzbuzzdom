d68d41b53810e70a8d1473c058e24309caa84b88
dinoarpa98 <dinoarpa98@gmail.com>
2022-03-14 19:01:22 +0100

    to end

---
+++ asset/main.js
@@ -0,0 +1,7 @@
+for( i=0; i<=1000; i++  ){
+  document.getElementById("ciclo").innerHTML += `
+    <div class="col text-center fs-2 p-3">
+      <div class="py-5 bg-primary">${i}</div>
+    </div>
+  `;
+}
