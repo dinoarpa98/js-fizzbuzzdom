999237ea93b8aba9e162545535bc98e3347ad7be
dinoarpa98 <dinoarpa98@gmail.com>
2022-03-15 18:19:51 +0100

    end

--- asset/main.js
+++ asset/main.js
@@ -1,7 +1,27 @@
-for( i=0; i<=1000; i++  ){
-  document.getElementById("ciclo").innerHTML += `
-    <div class="col text-center fs-2 p-3">
-      <div class="py-5 bg-primary">${i}</div>
-    </div>
+let container = document.getElementById("container_1");
+
+for (let i = 1; i <= 1000; i++) {
+
+
+  if (i % 3 == 0 && i % 5 == 0) {
+    let Fizzbuzz = "Fizzbuzz";
+    container.innerHTML += `
+      <div class="ciclo fizzbuzz">${Fizzbuzz}</div>
   `;
+  } else if (i % 3 == 0) {
+
+    let fizz = "Fizz";
+    container.innerHTML += `
+      <div class="ciclo fizz">${fizz}</div>
+  `;
+
+  } else if (i % 5 == 0) {
+
+     let buzz = "Buzz";
+     container.innerHTML +=
+     `<div class="ciclo buzz">${buzz}</div> `;
+
+  } else {
+    container.innerHTML += `<div class="ciclo" >${i}</div>`;
+  }
 }
